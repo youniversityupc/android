72c561edef1f71e12382291c16ba9e5f32dea39a
Angel Meneses <angelmenesest123@gmail.com>
2022-11-10 10:34:03 -0500

    fix: updated activity card dependencies

--- lib/pages/activities/components/activity_card.dart
+++ lib/pages/activities/components/activity_card.dart
@@ -1,57 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:youniversity_app/components/info_card.dart';
+import 'package:youniversity_app/pages/activities/models/activity_model.dart';
 import 'package:youniversity_app/utils/build_context_extensions.dart';
-import 'package:youniversity_app/utils/widget_list_extensions.dart';
 import 'package:youniversity_app/utils/text_style_extensions.dart';
 
 class ActivityCard extends StatelessWidget {
-  final String taskName;
-  final String courseName;
-  final String remainingTime;
-  final String topicTheme;
-  final Color backgroundColor;
+  final ActivityModel activity;
 
-  const ActivityCard({
-    super.key,
-    required this.taskName,
-    required this.courseName,
-    required this.remainingTime,
-    required this.topicTheme,
-    required this.backgroundColor,
-  });
-
-  Widget createTextWithIcon(BuildContext context, String label, IconData icon) {
-    return Row(
-      children: [
-        Icon(icon, color: Colors.white),
-        Text(
-          label,
-          style: context.textTheme.bodyLarge?.withColor(Colors.white),
-        ),
-      ].withHorizontalSpace(8),
-    );
-  }
+  const ActivityCard({super.key, required this.activity});
 
   @override
   Widget build(BuildContext context) {
     final textTheme = context.textTheme;
-
-    return Card(
-      color: backgroundColor,
-      child: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text(
-              taskName,
-              style: textTheme.headlineSmall?.withColor(Colors.white),
-            ),
-            createTextWithIcon(context, courseName, Icons.book),
-            createTextWithIcon(context, remainingTime, Icons.schedule),
-            createTextWithIcon(context, topicTheme, Icons.notes),
-          ].withVerticalSpace(8),
+    return InfoCard(
+      backgroundColor: activity.backgroundColor,
+      foregroundColor: Colors.white,
+      children: [
+        InfoCardItem(
+          child: Text(
+            activity.taskName,
+            style: textTheme.headlineSmall?.withColor(Colors.white),
+          ),
         ),
-      ),
+        InfoCardItem(
+          leading: const Icon(Icons.book),
+          child: Text(
+            activity.courseName,
+          ),
+        ),
+        InfoCardItem(
+          leading: const Icon(Icons.schedule),
+          child: Text(
+            activity.remainingTime,
+          ),
+        ),
+        InfoCardItem(
+          leading: const Icon(Icons.notes),
+          child: Text(
+            activity.topicTheme,
+          ),
+        ),
+      ],
     );
   }
 }
