612479d2fef3df45f9158109f7aa5ed7c92371e3
Diego Albitres <diego.albitres@gmail.com>
2022-11-10 07:36:14 -0500

    refactor: rewrite CreateTeacherPage using new widgets

--- lib/pages/courses/create_teacher_page.dart
+++ lib/pages/courses/create_teacher_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:youniversity_app/layout/app_theme.dart';
+import 'package:youniversity_app/components/profile_avatar.dart';
+import 'package:youniversity_app/utils/widget_list_extensions.dart';
 
 class CreateTeacherPage extends StatefulWidget {
   const CreateTeacherPage({super.key});
@@ -9,70 +10,48 @@ class CreateTeacherPage extends StatefulWidget {
 }
 
 class _CreateTeacherPageState extends State<CreateTeacherPage> {
-  @override
-  Widget build(BuildContext context) {
-    return ListView(
-      padding: const EdgeInsets.all(16.0),
-      children: [
-        RawMaterialButton(
-          onPressed: () {},
-          elevation: 1.0,
-          fillColor: const Color.fromARGB(255, 158, 203, 231),
-          padding: const EdgeInsets.all(32.0),
-          shape: const CircleBorder(),
-          child: const Icon(
-            Icons.add_a_photo,
-            size: 64.0,
-            color: Colors.white,
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: teacherName(),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: teacherEmail(),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: teacherPhone(),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: MaterialButton(
-            onPressed: () {},
-            color: AppColorPalette.primaryColor,
-            child: const Text(
-              'GUARDAR',
-              style: TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
-            ),
-          ),
-        ),
-      ],
+  Widget createTextInput({
+    required String label,
+    String? hint,
+    TextInputType keyboardType = TextInputType.text,
+    bool obscureText = false,
+  }) {
+    return TextField(
+      decoration: InputDecoration(
+        labelText: label,
+        hintText: hint,
+      ),
+      keyboardType: keyboardType,
+      obscureText: obscureText,
     );
   }
 
-  Widget teacherName() => const TextField(
-        decoration: InputDecoration(
-            hintText: 'David Quevedo',
-            labelText: 'Nombre del docente',
-            border: OutlineInputBorder()),
-        keyboardType: TextInputType.name,
-      );
-
-  Widget teacherEmail() => const TextField(
-        decoration: InputDecoration(
-            hintText: 'user@upc.edu.pe',
-            labelText: 'Correo institucional',
-            border: OutlineInputBorder()),
-        keyboardType: TextInputType.emailAddress,
-      );
-  Widget teacherPhone() => const TextField(
-        decoration: InputDecoration(
-            hintText: '999 999 999',
-            labelText: 'Teléfono (opcional)',
-            border: OutlineInputBorder()),
-        keyboardType: TextInputType.phone,
-      );
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        children: [
+          ProfileAvatar(
+            backgroundImage: const AssetImage('assets/profe-quevedo.jpg'),
+            child: InkWell(onTap: () {}),
+          ),
+          Column(
+            children: [
+              createTextInput(
+                label: 'Nombre del docente',
+                hint: 'John Doe',
+              ),
+              createTextInput(
+                label: 'Correo institucional',
+                hint: 'john.doe@gmail.com',
+                keyboardType: TextInputType.emailAddress,
+              ),
+              createTextInput(label: 'Teléfono', hint: 'Opcional'),
+            ].withVerticalSpace(16),
+          ),
+        ].withVerticalSpace(32),
+      ),
+    );
+  }
 }
