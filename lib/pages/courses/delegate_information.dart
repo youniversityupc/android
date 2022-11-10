34bdbe972d429cd61ff9ab20a68b36623d76169f
Diego Albitres <diego.albitres@gmail.com>
2022-11-10 09:26:21 -0500

    refactor: rewrite `DelegateInformation` using new widgets

--- lib/pages/courses/delegate_information.dart
+++ lib/pages/courses/delegate_information.dart
@@ -1,43 +1,39 @@
 import 'package:flutter/material.dart';
-import 'package:youniversity_app/components/course_card.dart';
-import 'package:youniversity_app/utils/app_colors.dart';
+import 'package:youniversity_app/components/info_card.dart';
+import 'package:youniversity_app/components/profile_avatar.dart';
+import 'package:youniversity_app/utils/widget_list_extensions.dart';
 
 class DelegateInformation extends StatelessWidget {
   const DelegateInformation({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
+    return Container(
+      padding: const EdgeInsets.all(16),
       child: Column(
         children: [
-          Container(
-            margin: const EdgeInsets.only(top: 16.0),
-            child: Column(
-              children: [
-                Container(
-                  width: 150,
-                  height: 150,
-                  decoration: const BoxDecoration(
-                    image: DecorationImage(
-                        image: AssetImage('assets/dalb.jpg'),
-                        fit: BoxFit.cover),
-                    borderRadius: BorderRadius.all(Radius.circular(120)),
-                  ),
-                ),
-                const Padding(
-                  padding: EdgeInsets.only(top: 16.0),
-                  child: InformationCard(
-                    color: Color.fromARGB(255, 32, 160, 117),
-                    name: 'Diego Albitres Blondet',
-                    email: 'u202015652@upc.edu.pe',
-                    phone: '999 999 999',
-                  ),
-                )
-              ],
-            ),
+          ProfileAvatar(
+            foregroundImage: const AssetImage('assets/dalb.jpg'),
           ),
-        ],
+          InfoCard(
+            backgroundColor: Colors.lightBlue,
+            foregroundColor: Colors.white,
+            children: const [
+              InfoCardItem(
+                leading: Icon(Icons.account_circle),
+                child: Text('Diego Albitres Blondet'),
+              ),
+              InfoCardItem(
+                leading: Icon(Icons.email),
+                child: Text('u202015652@upc.edu.pe'),
+              ),
+              InfoCardItem(
+                leading: Icon(Icons.phone),
+                child: Text('+51 999 999 999'),
+              ),
+            ],
+          )
+        ].withVerticalSpace(32),
       ),
     );
   }
