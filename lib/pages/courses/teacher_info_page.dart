dc5437ea2a746de4c5739eec0942374968bfab80
Diego Albitres <diego.albitres@gmail.com>
2022-11-10 07:05:20 -0500

    refactor: rewrite TeacherInfoPage using new widgets

--- lib/pages/courses/teacher_info_page.dart
+++ lib/pages/courses/teacher_info_page.dart
@@ -1,39 +1,35 @@
 import 'package:flutter/material.dart';
-import 'package:youniversity_app/components/course_card.dart';
-import 'package:youniversity_app/layout/app_theme.dart';
+import 'package:youniversity_app/components/info_card.dart';
+import 'package:youniversity_app/components/profile_avatar.dart';
+import 'package:youniversity_app/utils/widget_list_extensions.dart';
 
 class TeacherInfoPage extends StatelessWidget {
   const TeacherInfoPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
+    return Container(
+      padding: const EdgeInsets.all(16),
       child: Column(
         children: [
-          Container(
-            margin: const EdgeInsets.only(top: 16),
-            child: Column(
-              children: [
-                Container(
-                  width: 150,
-                  height: 150,
-                  decoration: const BoxDecoration(
-                    image: DecorationImage(
-                        image: AssetImage('assets/profe-quevedo.jpg'),
-                        fit: BoxFit.cover),
-                    borderRadius: BorderRadius.all(Radius.circular(120)),
-                  ),
-                ),
-                const InformationCard(
-                  color: AppColorPalette.primaryColor,
-                  name: 'David Gerardo Quevedo Velasco',
-                  email: 'pcsidque@upc.edu.pe',
-                ),
-              ],
-            ),
+          ProfileAvatar(
+            foregroundImage: const AssetImage('assets/profe-quevedo.jpg'),
           ),
-        ],
+          InfoCard(
+            backgroundColor: Colors.orange,
+            foregroundColor: Colors.white,
+            children: const [
+              InfoCardItem(
+                leading: Icon(Icons.badge_outlined),
+                child: Text('David Gerardo Quevedo Velasco'),
+              ),
+              InfoCardItem(
+                leading: Icon(Icons.email),
+                child: Text('pcsidque@upc.edu.pe'),
+              ),
+            ],
+          ),
+        ].withVerticalSpace(32),
       ),
     );
   }
