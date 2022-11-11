dc69d7b778dd270ef588b8649b907a444211909f
Diego Albitres <diego.albitres@gmail.com>
2022-11-11 04:47:17 -0500

    refactor: use new widgets in ProfileSettingsPage

--- lib/pages/profile/profile_settings_page.dart
+++ lib/pages/profile/profile_settings_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:youniversity_app/components/info_card.dart';
+import 'package:youniversity_app/components/profile_avatar.dart';
 import 'package:youniversity_app/layout/app_theme.dart';
 import 'package:youniversity_app/utils/build_context_extensions.dart';
 import 'package:youniversity_app/utils/text_style_extensions.dart';
@@ -39,26 +41,37 @@ class _ProfileSettingsPageState extends State<ProfileSettingsPage> {
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Container(
-        margin: const EdgeInsets.symmetric(vertical: 32),
-        padding: const EdgeInsets.symmetric(horizontal: 16),
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 32),
         child: Column(
           children: [
-            createAvatar(),
-            createListItem(
-              label: 'Nicola Casanova',
-              icon: Icons.account_circle,
+            ProfileAvatar(
+              backgroundImage: const AssetImage('assets/dalb.jpg'),
+              child: InkWell(onTap: () {}),
             ),
-            createListItem(
-              label: 'u201812345@upc.edu.pe',
-              icon: Icons.email,
-            ),
-            createListItem(
-              label: '+51 999 999 999',
-              icon: Icons.phone,
-            ),
-            createListItem(
-              label: 'Universidad Peruana de Ciencias Aplicadas',
-              icon: Icons.school,
+            InfoCard(
+              backgroundColor: Colors.transparent,
+              foregroundColor: AppColorPalette.primaryColor,
+              elevation: 0,
+              contentVerticalGap: 32,
+              padding: EdgeInsets.zero,
+              children: const [
+                InfoCardItem(
+                  leading: Icon(Icons.account_circle),
+                  child: Text('Nicola Casanova'),
+                ),
+                InfoCardItem(
+                  leading: Icon(Icons.email),
+                  child: Text('u201812345@upc.edu.pe'),
+                ),
+                InfoCardItem(
+                  leading: Icon(Icons.phone),
+                  child: Text('+51 999 999 999'),
+                ),
+                InfoCardItem(
+                  leading: Icon(Icons.school),
+                  child: Text('Universidad Peruana de Ciencias Aplicadas'),
+                ),
+              ],
             ),
           ].withVerticalSpace(32),
         ),
